e37c6bb15dce6f0e54a2c891389fdb186650410c
NeiltonSeguins <seguins.neilton@gmailcom>
2022-10-20 19:55:26 -0300

    Subindo código fora de qualidade

--- src/componentes/Checkbox/index.jsx
+++ src/componentes/Checkbox/index.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function Checkbox() {
   return (
-    <div>
-      <label htmlFor="checkbox">Check</label>
-      <input id="checkbox" type="checkbox" />
+    <div><label htmlFor="checkbox">Check</label>
+<input id="checkbox" type="checkbox" />
     </div>
   );
 }
