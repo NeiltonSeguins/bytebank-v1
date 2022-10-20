f2ccba9827725dac4d5ef6804592e4160a3166cd
NeiltonSeguins <seguins.neilton@gmailcom>
2022-10-20 20:05:56 -0300

    Testando o lint

--- src/componentes/Checkbox/index.jsx
+++ src/componentes/Checkbox/index.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Checkbox() {
   return (
-    <div><label htmlFor="checkbox">Check</label>
-<input id="checkbox" type="checkbox" />
+    <div>
+      <label htmlFor="checkbox">Check</label>
+      <input id="checkbox" type="checkbox" />
     </div>
   );
 }
