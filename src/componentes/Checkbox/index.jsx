6094b2e72c0b70697a5eca94b1dd7de9218cda72
NeiltonSeguins <seguins.neilton@gmailcom>
2022-10-20 20:09:24 -0300

    Enviando código ruim

--- src/componentes/Checkbox/index.jsx
+++ src/componentes/Checkbox/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Checkbox() {
   return (
     <div>
-      <label htmlFor="checkbox">Check</label>
-      <input id="checkbox" type="checkbox" />
+<label htmlFor="checkbox">Check</label>
+<input id="checkbox" type="checkbox" />
     </div>
   );
 }
