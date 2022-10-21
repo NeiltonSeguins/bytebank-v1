f989a42f5d445d7d767b75de239167a8f4890e88
NeiltonSeguins <seguins.neilton@gmailcom>
2022-10-20 21:27:26 -0300

    testes passando

--- src/componentes/Checkbox/Checkbox.test.js
+++ src/componentes/Checkbox/Checkbox.test.js
@@ -4,7 +4,7 @@ import Checkbox from './index';
 
 test('Deve renderizar um checkbox que pode ser clicado', () => {
   render(<Checkbox />);
-  const input = screen.getByLabelText('Checko');
+  const input = screen.getByLabelText('Check');
   userEvent.click(input);
   expect(input).toBeChecked();
 });
