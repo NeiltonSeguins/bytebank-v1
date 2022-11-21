f43b64a06d9be12c75da4244a78317f5f4a8d89b
NeiltonSeguins <seguins.neilton@gmail.com>
2022-11-21 10:48:52 -0300

    Adiciona mocks de chamadas com status OK e status com erro para a api

--- src/AppIntegracoes.test.js
+++ src/AppIntegracoes.test.js
@@ -1,22 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './rotas/App';
+import api from './services/api';
 import { buscaTransacoes } from './services/transacoes';
 
-// jest.mock('./services/api');
+jest.mock('./services/api');
+
+// Mock de retorno de dado da api
+const mockTransacao = [
+  {
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '18/11/2022',
+    mes: 'Novembro',
+  },
+];
+
+// Função que simula uma chamada e retorno a api
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno,
+      });
+    }, 200);
+  });
+};
+
+// Função que simula o retorno de uma chamada que deu erro em uma api
+const mockRequisicaoErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
+};
 
 describe('Realizando requisições para API', () => {
-  test('Deve exibir uma lista de transações puxadas da API', async () => {
+  test('Deve exibir uma lista de transações', async () => {
+    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
     const transacoes = await buscaTransacoes();
 
-    render(<App />, { wrapper: BrowserRouter });
-    const transacao = await screen.findAllByText('Novembro');
-    transacao.forEach((el) => {
-      expect(el).toBeInTheDocument();
-    });
-    const listaTransacoes =
-      screen.getByTestId('lista-transacoes').children.length;
-    expect(listaTransacoes).toBe(3);
-    expect(transacoes).toHaveLength(3);
+    expect(transacoes).toEqual(mockTransacao);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+    expect(api.get).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+    api.get.mockImplementation(() => mockRequisicaoErro());
+    const transacoes = await buscaTransacoes();
+
+    expect(transacoes).toEqual([]);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+    expect(api.get).toHaveBeenCalledTimes(1);
   });
 });
