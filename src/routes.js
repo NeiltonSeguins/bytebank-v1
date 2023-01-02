dcd947398ab665eb8c09d59543312ead534d071a
NeiltonSeguins <seguins.neilton@gmailcom>
2023-01-02 16:41:09 -0300

    Adiciona componente de rodapé e página de início da aplicação

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import Cartoes from './componentes/Cartoes';
 import Investimentos from './componentes/Investimentos';
 import Servicos from './componentes/Servicos';
+import Inicio from './paginas/Inicio';
 import Pagina404 from './paginas/Pagina404';
 import App from './paginas/Principal/App';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path="/" element={<Inicio />} />
+      {/* <Route path="/" element={<App />}>
         <Route path="cartoes" element={<Cartoes />} />
         <Route path="investimentos" element={<Investimentos />} />
         <Route path="servicos" element={<Servicos />} />
-      </Route>
+      </Route> */}
 
       <Route path="*" element={<Pagina404 />} />
     </Routes>
